54ba0bb6ac5f929fd716b6fcc1a5749177374976
rubynbasnyat <141rubynbasnyat@gmail.com>
2023-03-31 08:52:29 +0545

    Add project files.

---
+++ abstract/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @abstract
+{
+    internal abstract class Calculatorabs
+    {
+        int a = 10;
+        int b = 20;
+        public abstract int c { get; set; }
+        public int sum(int a, int b) 
+        {
+            return a + b;
+        }
+    }
+    class test
+    { 
+        static void Main(string[] args)
+        {
+        }
+    }
+}
